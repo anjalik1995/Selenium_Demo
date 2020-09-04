a0e2cf9ce54ac392dee664ca57d77720e662c39a
Aspire <Aspire@Aspire-PC>
2020-09-04 20:38:10 +0530

    Commit Selenium_Webdriver_OpenBrowser_CloseBrowser

--- src/DemoLaunch.java
+++ src/DemoLaunch.java
@@ -1,10 +1,13 @@
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class DemoLaunch {
 	public static void main(String[] args) 
 	{
 		System.setProperty("webdriver.chrome.driver",".\\software\\chromedriver.exe");
-		ChromeDriver driver = new ChromeDriver();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.close();
 	}
 
 }
